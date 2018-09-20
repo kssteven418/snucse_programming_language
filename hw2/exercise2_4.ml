d8462372413c2371862163b3ec14683412abaa9b
Steven Kim <ksteven1995@gmail.com>
2018-09-20 19:25:56 +0900

    hw2 : exercise 4 fixed according to the correct spec

--- hw2/exercise2_4.ml
+++ hw2/exercise2_4.ml
@@ -1,5 +1,6 @@
 
 
+
 module type Queue =
 sig
 	type element
@@ -13,117 +14,108 @@ end
 module IntListQ =
 struct
 	type element = int list
-	type queue = (int list * int list)
+	type queue = (int list list * int list list)
+	
 	exception EMPTY_Q
 	
 	let emptyQ = ([], [])
 	
 	let enQ = fun (q, e) ->
-		let (lq, rq) = q in
-			(* reverse the order of the input element list e and append to the left queue *)
-			let rec rev inlist =
-				if List.length inlist == 0 then []
-				else (rev (List.tl inlist)) @ [List.hd inlist] in
-			((rev e)@lq, rq) 
+		let (pri, sec) = q in
+			([e] @ pri, sec) 
 	
-	let deQ = fun q ->
-		let (lq, rq)= q in
+	let deQ = fun q -> 
+		let (pri, sec) = q in
+			(* if the secondary stack is not full *)
+			if (List.length sec) != 0 then
+				((List.hd sec), (pri, List.tl sec))
+			
+			(* if both the primary and the secondary queues are empty *)
+			(* then, raise the exception *)
+			else if (List.length pri) == 0 then
+				raise EMPTY_Q
 
-			(*if right queue is not empty, dequeue from the low index!*)
-			if (List.length rq) != 0 then
-				([List.hd rq], (lq, List.tl rq))
-			
-			(*if left, right queue are both empty, exception!*)
-			else if (List.length lq) == 0 then raise EMPTY_Q
-			
-			(* if only right queue is empty, move the elmts from the left queue to the right *)
-			else let rec move left =
-				if (List.length left == 0) then []
-				else  move(List.tl left) @ [List.hd left] in
-				([List.hd (move lq)], ([], List.tl (move lq)))
-				
-				
-				
-				
+			(* if only the secondary queue is empty *)
+			(* then, first move all the elmt from the pri to the sec *)
+			else let rec move p = 
+				if (List.length p) == 0 then []
+				else move(List.tl p) @ [List.hd p] in
+				(List.hd (move pri), ([], List.tl (move pri)))
 end
 
-(*debugging*)	
+let printList lis = 
+	let rec printRec l =
+		if (List.length l) == 0 then ""
+		else if (List.length l) == 1 then (string_of_int (List.hd l))
+		else (string_of_int (List.hd l)) ^ ";" ^ (printRec (List.tl l)) in
+	"[" ^ (printRec lis) ^ "]"
+	
 
 let toString q = 
-	let (left, right)=q in
-		let rec print q =
-			if (List.length q) == 0 then ""
-			else (string_of_int (List.hd q)) ^ " " ^ (print (List.tl q)) in
-		(print left) ^ "| " ^ (print right)  
-
-let li = IntListQ.emptyQ
-let _ = print_endline (toString li)
-let li = IntListQ.enQ(li, [1])
-let _ = print_endline (toString li)
-let li = IntListQ.enQ(li, [2; 3; 4])
-let _ = print_endline (toString li)
-let (x, li) = IntListQ.deQ(li)
-let _ = print_endline (string_of_int (List.hd x))
-let _ = print_endline (toString li)
-let (x, li) = IntListQ.deQ(li)
-let _ = print_endline (string_of_int (List.hd x))
-let _ = print_endline (toString li)
+	let (p, s) = q in
+		let rec printQ queue =
+			if (List.length queue) == 0 then ""
+			else if (List.length queue) == 1 
+				then (printList (List.hd queue)) 
+			else "" ^ (printList (List.hd queue)) ^ ";" ^ (printQ (List.tl queue)) ^ ""  in
+		"([" ^ (printQ p) ^ "], [" ^ (printQ s) ^ "])"
 
 
-let li = IntListQ.enQ(li, [5; 6; 7])
-let _ = print_endline (toString li)
-let (x, li) = IntListQ.deQ(li)
-let _ = print_endline (string_of_int (List.hd x))
-let _ = print_endline (toString li)
-let li = IntListQ.enQ(li, [8; 9])
-let _ = print_endline (toString li)
+let q = IntListQ.emptyQ
+let _ = print_endline (toString q)
 
-let (x, li) = IntListQ.deQ(li)
-let _ = print_endline (string_of_int (List.hd x))
-let _ = print_endline (toString li)
-let (x, li) = IntListQ.deQ(li)
-let _ = print_endline (string_of_int (List.hd x))
-let _ = print_endline (toString li)
-let (x, li) = IntListQ.deQ(li)
-let _ = print_endline (string_of_int (List.hd x))
-let _ = print_endline (toString li)
-let (x, li) = IntListQ.deQ(li)
-let _ = print_endline (string_of_int (List.hd x))
-let _ = print_endline (toString li)
-
-let li = IntListQ.enQ(li, [10])
-let _ = print_endline (toString li)
-let (x, li) = IntListQ.deQ(li)
-let _ = print_endline (string_of_int (List.hd x))
-let _ = print_endline (toString li)
-let (x, li) = IntListQ.deQ(li)
-let _ = print_endline (string_of_int (List.hd x))
-let _ = print_endline (toString li)
+let q = IntListQ.enQ(IntListQ.emptyQ, [1;2;3])
+let _ = print_endline (toString q)
 
 
-let (x, li) = IntListQ.deQ(li)
-let _ = print_endline (string_of_int (List.hd x))
-let _ = print_endline (toString li)
-let (x, li) = IntListQ.deQ(li)
-let _ = print_endline (string_of_int (List.hd x))
-let _ = print_endline (toString li)
-(*
-
-let rec printList : int list  -> string = fun (l) ->
-	if List.length l == 0 then " "
-	else (string_of_int (List.hd l)) ^ " " ^ (printList (List.tl l))
-module ValidIntListQ = (IntListQ : Queue)
-
-let li = IntListQ.emptyQ
-let _ = print_endline (printList li)
-let li = IntListQ.enQ(li, [1])
-let _ = print_endline (printList li)
-let li = IntListQ.enQ(li, [2; 3; 4])
-let _ = print_endline (printList li)
-let li = IntListQ.emptyQ
-let _ = print_endline (printList li)
+let q = IntListQ.enQ(q, [4;5])
+let _ = print_endline (toString q)
 
 
-let _ = print_endline (printList [1; 2; 3; 4])
-*)
+let (n, q) = IntListQ.deQ(q)
+let _ = print_endline (printList n)
+let _ = print_endline (toString q)
+
+let q = IntListQ.enQ(q, [6; 7])
+let _ = print_endline (toString q)
+
+let q = IntListQ.enQ(q, [8; 9])
+let _ = print_endline (toString q)
+
+let q = IntListQ.enQ(q, [10])
+let _ = print_endline (toString q)
+
+let (n, q) = IntListQ.deQ(q)
+let _ = print_endline (printList n)
+let _ = print_endline (toString q)
+
+let (n, q) = IntListQ.deQ(q)
+let _ = print_endline (printList n)
+let _ = print_endline (toString q)
+
+let q = IntListQ.enQ(q, [11;12;13])
+let _ = print_endline (toString q)
+
+let q = IntListQ.enQ(q, [14])
+let _ = print_endline (toString q)
+
+let (n, q) = IntListQ.deQ(q)
+let _ = print_endline (printList n)
+let _ = print_endline (toString q)
+
+let (n, q) = IntListQ.deQ(q)
+let _ = print_endline (printList n)
+let _ = print_endline (toString q)
+
+let (n, q) = IntListQ.deQ(q)
+let _ = print_endline (printList n)
+let _ = print_endline (toString q)
+
+let (n, q) = IntListQ.deQ(q)
+let _ = print_endline (printList n)
+let _ = print_endline (toString q)
+
+let (n, q) = IntListQ.deQ(q)
+let _ = print_endline (printList n)
+let _ = print_endline (toString q)
 
