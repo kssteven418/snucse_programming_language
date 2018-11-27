cdba91b6bbd36662f64cb49c77712ce251821c07
stevenKim <ksteven19950418@gmail.com>
2018-11-28 03:46:47 +0900

    hw6 : ex4 solving equation done. TODO : type constraints and output

--- hw6/ex4/simple_checker.ml
+++ hw6/ex4/simple_checker.ml
@@ -58,9 +58,11 @@ type typ =
   | TLoc of typ
   | TFun of typ * typ
   | TVar of var
-	| TIBS (* int, bool, string *)
-	| TIBSL of typ (* int, bool, string, loc *)
   (* Modify, or add more if needed *)
+
+type tcon = 
+	| IBS
+	| IBSL
 
 let rec str_of_type t = 
 	match t with 
@@ -73,8 +75,6 @@ let rec str_of_type t =
   | TFun (t1, t2) -> 
 		"("^(str_of_type t1)^"->"^(str_of_type t2)^")"
   | TVar v -> v
-	| TIBS -> "(Int, Bool, String)"
-	| TIBSL v -> "(Int, Bool, String, "^(str_of_type v)^")"
 
 let print_type t =
 	let _ = print_endline (str_of_type t) in
@@ -102,6 +102,9 @@ let check : M.exp -> M.types = fun exp ->
 
 
 	(* BUILD EQUATIONS *)
+
+	(* stores type constraints *)
+	let type_constraint = ref [] in
 
 	let rec v : glist * M.exp * typ -> equ list = fun (gamma, e, tau) ->
 		match e with
@@ -156,9 +159,9 @@ let check : M.exp -> M.types = fun exp ->
 				| M.SUB ->
 					[(tau, TInt)] @ v(gamma, e1, TInt) @ v(gamma, e2, TInt)
 				| M.EQ -> 
-					let tau_loc = new_var() in
 					let tau' = new_var() in
-					[(TVar(tau'), TIBSL(TLoc(TVar(tau_loc))))] @
+					let _ = type_constraint := 
+						(!type_constraint)@[(TVar(tau'), IBSL)] in
 					v(gamma, e1, TVar(tau')) @
 					v(gamma, e2, TVar(tau')) @
 					[(tau, TBool)]
@@ -169,10 +172,10 @@ let check : M.exp -> M.types = fun exp ->
 			)
 
 		| M.READ -> [(tau, TInt)]
-		
 
 		| M.WRITE e -> 
-			[(tau, TIBS)] @
+				let _ = type_constraint := 
+					(!type_constraint)@[(tau, IBS)] in
 			v(gamma, e, tau)
 		
 		| M.MALLOC e ->
@@ -231,23 +234,14 @@ let check : M.exp -> M.types = fun exp ->
 	let rec iterate (eq, decl, ans) = 
 		if (List.length eq)=0 then []
 		else 
-			(* TVar and TTypes must be the fst element *)
+			(* TVar must be the fst element *)
 			(* if tie, then TVar is the fst elmt, and the TTypes is the snd *)
 			let (t1, t2) = (List.hd eq) in
-			(*
-			let (t1, t2) =
-			match t2 with 
-				| TTypes tt -> (t2, t1)
-				| _ -> (t1, t2) in
-			*)
 			let (t1, t2) =
 			match t2 with 
 				| TVar v -> (t2, t1)
 				| _ -> (t1, t2) in
-			(*
-				let _ = print_equ [(t1, t2)] in
-			iterate((List.tl eq), ans) 
-			*)
+
 			match t1 with
 				| TInt -> 
 				(match t2 with 
@@ -284,19 +278,39 @@ let check : M.exp -> M.types = fun exp ->
 				 	| TFun (f1', f2') -> [(f1, f1');(f2, f2')] @ iterate((List.tl eq), decl, ans)
 					| _ -> raise(M.TypeError "Expecting function")  
 				)
+
 				| TVar v ->
-					(* if not declared yet *)
+					(* 1. 1st is not declared yet *)
 					if (decl v) = 0 then 
-						let (decl', ans') = declare (decl, ans, v, t2) in 
-						let _ = g_decl := decl' in
-						let _ = g_ans := ans' in
-						iterate((List.tl eq), decl', ans')
-					(* if declared *)
+						(match t2 with
+						 	(* 1.1 2nd is undeclared variable is not defined *)
+						 	| TVar v' ->
+								if v = v' then iterate((List.tl eq), decl, ans)
+								else if (decl v') = 0 then 
+									let (decl', ans') = declare (decl, ans, v, t2) in 
+									let _ = g_decl := decl' in
+									let _ = g_ans := ans' in
+									iterate((List.tl eq), decl', ans')
+								(* 1.2 2nd is declared variable, than swap the position *)
+								else iterate([(t1, t2)]@(List.tl eq), decl, ans) 
+							(* 1.3 2nd is not variable *)
+							| _ ->
+								let (decl', ans') = declare (decl, ans, v, t2) in 
+								let _ = g_decl := decl' in
+								let _ = g_ans := ans' in
+								iterate((List.tl eq), decl', ans')
+						)
+					(* 2. 1st is declared *)
 					else
 						[(ans v, t2)] @ iterate((List.tl eq), decl, ans)
-
 	in
 
+	let rec solve_equation eq = 	
+		if (List.length eq) = 0 then ()
+		else let e = iterate (eq, !g_decl, !g_ans) in
+			solve_equation e in
+
+	(* debuggin purpose *)
 	let rec print_ans vl = 
 		if (List.length vl)=0 then ()
 		else let v = (List.hd vl) in
@@ -305,15 +319,56 @@ let check : M.exp -> M.types = fun exp ->
 				let _ = print_type (!g_ans v) in (print_ans (List.tl vl))
 			else (print_ans (List.tl vl)) in
 
-	let decl_init = fun x ->  0 in
-	let ans_init = fun x -> raise UnbindError in
-
+	let _ = solve_equation equations in
 	let _ = print_equ equations in
-  let e' = iterate (equations, decl_init, ans_init) in
+	
+	let _ = print_endline "==ANS==" in
+	let _ = print_ans !var_list in
+	let _ = print_endline "" in
+	raise (M.TypeError "Type Checker Unimplemented")
+	(*
+  let e' = iterate (equations, !g_decl, !g_ans) in
 	let _ = print_endline "==ANS==" in
 	let _ = print_ans !var_list in
 	let _ = print_endline "" in
 	let _ = print_equ e' in
 	
+	let _ = print_endline "" in
+  let e' = iterate (e', !g_decl, !g_ans) in
+	let _ = print_endline "==ANS==" in
+	let _ = print_ans !var_list in
+	let _ = print_endline "" in
+	let _ = print_equ e' in
+	let _ = print_endline "" in
+  let e' = iterate (e', !g_decl, !g_ans) in
+	let _ = print_endline "==ANS==" in
+	let _ = print_ans !var_list in
+	let _ = print_endline "" in
+	let _ = print_equ e' in
+	let _ = print_endline "" in
+  let e' = iterate (e', !g_decl, !g_ans) in
+	let _ = print_endline "==ANS==" in
+	let _ = print_ans !var_list in
+	let _ = print_endline "" in
+	let _ = print_equ e' in
+	let _ = print_endline "" in
+  let e' = iterate (e', !g_decl, !g_ans) in
+	let _ = print_endline "==ANS==" in
+	let _ = print_ans !var_list in
+	let _ = print_endline "" in
+	let _ = print_equ e' in
+	let _ = print_endline "" in
+  let e' = iterate (e', !g_decl, !g_ans) in
+	let _ = print_endline "==ANS==" in
+	let _ = print_ans !var_list in
+	let _ = print_endline "" in
+	let _ = print_equ e' in
+	let _ = print_endline "" in
+  let e' = iterate (e', !g_decl, !g_ans) in
+	let _ = print_endline "==ANS==" in
+	let _ = print_ans !var_list in
+	let _ = print_endline "" in
+	let _ = print_equ e' in
 	raise (M.TypeError "Type Checker Unimplemented")
+	*)
 
